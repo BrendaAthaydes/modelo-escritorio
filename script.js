e064f2a2fa70470f49325c9185f2f29c17367a1d
Brenda Athaydes <109760998+BrendaAthaydes@users.noreply.github.com>
2025-04-13 22:27:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Função chamada ao enviar o formulário
+function enviarFormulario() {
+    alert("Mensagem enviada com sucesso! Entraremos em contato em breve.");
+    return false; // Evita recarregar a página
+  }
+  
