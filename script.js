8e0319900f89782d954058d37353eddbbc976a29
Brenda Athaydes <109760998+BrendaAthaydes@users.noreply.github.com>
2025-04-15 02:08:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Função chamada ao enviar o formulário
-function enviarFormulario() {
-    alert("Mensagem enviada com sucesso! Entraremos em contato em breve.");
-    return false; // Evita recarregar a página
-  }
-  
